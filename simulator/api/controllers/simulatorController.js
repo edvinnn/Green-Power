38b580a983bfa8870ead9b81900b755d04595431
edvinnn <sladic.edvin@gmail.com>
2019-11-05 11:39:31 +0100

    added som hardcoded response on GET

--- simulator/api/controllers/simulatorController.js
+++ simulator/api/controllers/simulatorController.js
@@ -12,4 +12,12 @@ exports.create_wind = function(req, res) {
     });
 }
 
+exports.get_wind = function(req, res) {
+    //var new_wind = new Wind(req, res);
+    //new_wind.save(function(err, wind) {
+    //    if (err)
+    //        res.send(err);
+    //    res.json(wind);
+    res.json({'text': 'hellow world'});
+}
 
--- simulator/api/routes/simulatorRoutes.js
+++ simulator/api/routes/simulatorRoutes.js
@@ -3,5 +3,5 @@
 module.exports = function(app) {
     var simulator = require('../controllers/simulatorController');
 
-    app.route('/wind').post(simulator.create_wind);
+    app.route('/wind').post(simulator.create_wind).get(simulator.get_wind);
 };
