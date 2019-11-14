8ab258f92a512693fca3437ed80aa16f67e8dc16
edvinnn <sladic.edvin@gmail.com>
2019-11-14 11:35:02 +0100

    forgot to merge routes last time around

--- server/api/routes.js
+++ server/api/routes.js
@@ -46,15 +46,9 @@ router.delete('/:id', (req, res) => {
 })
 
 // Get all consumer consumptions
-<<<<<<< HEAD
 router.get('/consumer/consumption', async (req, res) => {
     try {
-        const consumptions = await Model.Consumption.find()
-=======
-router.get('/consumption', async (req, res) => {
-    try {
         const consumptions = await Model.Consumer.find()
->>>>>>> 794c91544dbabccf7d31f6e4abcb4c12a3a8c4cf
         res.json(consumptions)
     } catch (err) {
         res.status(500).json({ message: err.message })
@@ -62,13 +56,8 @@ router.get('/consumption', async (req, res) => {
 })
 
 // Get latest consumption
-<<<<<<< HEAD
 router.get('/consumer/consumption/latest', (req, res) => {
-    Model.Consumption.find().sort({_id:-1}).limit(1).exec(function(err, consumption){
-=======
-router.get('/consumption/latest', (req, res) => {
     Model.Consumer.find().sort({_id:-1}).limit(1).exec(function(err, consumption){
->>>>>>> 794c91544dbabccf7d31f6e4abcb4c12a3a8c4cf
         try {
             res.status(200).json({consumption})
         } catch (err) {
@@ -79,13 +68,8 @@ router.get('/consumption/latest', (req, res) => {
 })
 
 // Create new consumption
-<<<<<<< HEAD
 router.post('/consumer/consumption', async (req, res) => {
-    const consumption = new Model.Consumption({
-=======
-router.post('/consumption', async (req, res) => {
     const consumption = new Model.Consumer({
->>>>>>> 794c91544dbabccf7d31f6e4abcb4c12a3a8c4cf
         consumption: req.body.consumption
     })
 
@@ -97,12 +81,9 @@ router.post('/consumption', async (req, res) => {
     }
 })
 
-<<<<<<< HEAD
 // Returns the total of all prosumers consumption
 router.get('/prosumer/consumption'), async (req, res) => {
     
 }
 
-=======
->>>>>>> 794c91544dbabccf7d31f6e4abcb4c12a3a8c4cf
 module.exports = router
