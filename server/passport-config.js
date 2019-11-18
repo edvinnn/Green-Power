2e03a3319dabbef3be370d0e39b67c7253c10f98
Edvin Sladic <edvinnn@users.noreply.github.com>
2019-11-18 14:26:22 +0100

    Register prosumer as user to monogdb (#18)
    * added bcrypt for hasing user passwords
    
    * updated prosumer model to hold name, email and password
    
    * added login and register routes and functionality to register
    
    * added bcrypt
    
    * Added functionality to sign in and sign out prosumer (#23)
    
    * Added functionality to sign in and sign out prosumer
    
    * fixed bug in deserializing user from db

---
+++ server/passport-config.js
@@ -0,0 +1,33 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByMail, getUserById) {
+
+    const authenticateUser = async (email, password, done) => {
+        const users = await getUserByMail(email)
+        const user = users[0] //Todo: Match the right user (itterate)
+        if (user != null){
+            try {
+                if (await bcrypt.compare(password, user.password)) {
+                    return done(null, user)
+                } else {
+                    return done(null, false, {message: 'Incorrect password'})
+                }
+            } catch (err) {
+                return done(err)
+            }
+        }
+        return done(null, false, {message: 'No user found with that email'})
+    }
+
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    
+    passport.serializeUser((user, done) => done(null, user._id))
+    passport.deserializeUser((id, done) => {
+        getUserById(id).then(function(user) {
+            return done(null, user)
+        })
+    })
+}
+
+module.exports = initialize
