8fd6a0e5596de36039c09e232c49553dd071da8f
Edvin Sladic <edvinnn@users.noreply.github.com>
2019-11-18 14:29:05 +0100

    added nodemon as dev-only package and added ejs (#16)
    * added nodemon as dev-only package and added ejs
    
    * Added views and website routes (#17)
    
    * added view folder with simple index and routes for webiste
    
    * Register prosumer as user to monogdb (#18)
    
    * added bcrypt for hasing user passwords
    
    * updated prosumer model to hold name, email and password
    
    * added login and register routes and functionality to register
    
    * added bcrypt
    
    * Added functionality to sign in and sign out prosumer (#23)
    
    * Added functionality to sign in and sign out prosumer
    
    * fixed bug in deserializing user from db

--- server/server.js
+++ server/server.js
@@ -1,17 +1,44 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const Model = require('./model')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => Model.Prosumer.find({email: email}).exec(),
+    id => Model.Prosumer.findById(id).exec()
+)
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.set('view-engine', 'ejs')
 
 // Connect to database
-mongoose.connect('mongodb://localhost/GreenPowerDB', { useNewUrlParser: true })
+mongoose.connect(process.env.SERVER_DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('server connected to database'))
+db.once('open', () => console.log('Server connected to: ' + process.env.SERVER_DATABASE))
 
 // Setup routes
-const routes = require('./api/routes')
-app.use('/api', routes)
+const api_routes = require('./api/routes')
+const web_routes = require('./website/routes')
+app.use('/api', api_routes)
+app.use('/', web_routes)
 
 app.listen(3000, () => console.log('server started'))
---
+++ server/website/routes.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const router = express.Router()
+const Model = require('./../model')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+router.get('/', async (req, res) => {
+    res.render('index.ejs', {name: 'Hello, world!'})
+})
+
+router.get('/profile', checkAuth, async (req, res) => {
+    res.render('index.ejs', {name: req.user.name})
+})
+
+router.get('/login', checkNotAuth, async (req, res) => {
+    res.render('login.ejs')
+})
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/profile', failureRedirect: '/login', failureFlash: true}))
+
+router.get('/register', checkNotAuth, async (req, res) => {
+    res.render('register.ejs')
+})
+
+router.post('/register', async (req, res) => {
+    try {
+        const hashed_pwd = await bcrypt.hash(req.body.password, 10)
+
+        const prosumer = new Model.Prosumer({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashed_pwd
+        })
+
+        await prosumer.save()
+
+        res.redirect('/login')
+    } catch (err) {
+        res.status(500)
+    }
+})
+
+router.get('/logout', checkAuth, (req, res) => {
+    req.logOut()
+    res.redirect('/')
+})
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuth(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+module.exports = router
