2b933ba5834b10a381acd01047be2e283302f497
Hampus Holmström <Hampe.holmstrom@gmail.com>
2019-12-02 16:35:50 +0100

    resolved requested changes

--- server/api/routes.js
+++ server/api/routes.js
@@ -4,8 +4,8 @@ const server_db_utils = require('../server_db_utils')
 const sim_db_utils = require('../../simulator/sim_db_utils')
 
 //CONSUMPTION
-//Update prosumer.js consumption by id
-router.put('/prosumer.js/:id/consumption', async (req, res) => {
+//Update prosumer consumption by id
+router.put('/prosumer/:id/consumption', async (req, res) => {
 
     try{
         const prosumer = await server_db_utils.updateProsumerConsumptionById(req.params.id, req.body.consumption)
@@ -20,7 +20,7 @@ router.put('/prosumer.js/:id/consumption', async (req, res) => {
 })
 
 // Returns the total of all prosumers consumption
-router.get('/prosumer.js/consumption', async (req, res) => {
+router.get('/prosumer/consumption', async (req, res) => {
     try {
         const prosumers = await server_db_utils.getAllProsumers()
         
@@ -35,8 +35,8 @@ router.get('/prosumer.js/consumption', async (req, res) => {
     }
 })
 
-// Get prosumer.js consumption by id
-router.get('/prosumer.js/:id/consumption', async (req, res) => {
+// Get prosumer consumption by id
+router.get('/prosumer/:id/consumption', async (req, res) => {
     try {
         const prosumer = await server_db_utils.getProsumerById(req.params.id)
         if(prosumer == null){
@@ -50,8 +50,8 @@ router.get('/prosumer.js/:id/consumption', async (req, res) => {
 })
 
 // PRODUCTION
-// Update prosumer.js production by id
-router.put('/prosumer.js/:id/production', async (req, res) => {
+// Update prosumer production by id
+router.put('/prosumer/:id/production', async (req, res) => {
     try{
         const prosumer = await server_db_utils.updateProsumerProductionById(req.params.id, req.body.production)
         if (prosumer == null) {
@@ -65,7 +65,7 @@ router.put('/prosumer.js/:id/production', async (req, res) => {
 })
 
 // Returns the total of all prosumers production
-router.get('/prosumer.js/production', async (req, res) => {
+router.get('/prosumer/production', async (req, res) => {
     try {
         const prosumers = await server_db_utils.getAllProsumers()
         let total_production = 0
@@ -80,8 +80,8 @@ router.get('/prosumer.js/production', async (req, res) => {
     }
 })
 
-// Get prosumer.js production by id
-router.get('/prosumer.js/:id/production', async (req, res) => {
+// Get prosumer production by id
+router.get('/prosumer/:id/production', async (req, res) => {
     try {
         const prosumer = await server_db_utils.getProsumerById(req.params.id)
         if (prosumer == null) {
@@ -95,8 +95,8 @@ router.get('/prosumer.js/:id/production', async (req, res) => {
 })
 
 // NET PRODUCTION
-// Get prosumer.js net production by id
-router.get('/prosumer.js/:id/net_production', async(req, res) => {
+// Get prosumer net production by id
+router.get('/prosumer/:id/net_production', async(req, res) => {
     try {
         const prosumer = await server_db_utils.getProsumerById(req.params.id)
         if(prosumer == null){
@@ -113,8 +113,8 @@ router.get('/prosumer.js/:id/net_production', async(req, res) => {
 } )
 
 // BUFFER
-// Get prosumer.js buffer by id
-router.get('/prosumer.js/:id/buffer', async (req, res) => {
+// Get prosumer buffer by id
+router.get('/prosumer/:id/buffer', async (req, res) => {
     try {
         const prosumer = await server_db_utils.getProsumerById(req.params.id)
         if(prosumer == null){
@@ -127,8 +127,8 @@ router.get('/prosumer.js/:id/buffer', async (req, res) => {
     }
 })
 
-// Update prosumer.js buffer by id
-router.put('/prosumer.js/:id/buffer', async (req, res) => {
+// Update prosumer buffer by id
+router.put('/prosumer/:id/buffer', async (req, res) => {
     const prosumer = await server_db_utils.updateProsumerBufferById(req.params.id, req.body.buffer)
     try{
         if(prosumer == null){
@@ -141,8 +141,8 @@ router.put('/prosumer.js/:id/buffer', async (req, res) => {
     }
 })
 
-// Get prosumer.js max buffer by id
-router.get('/prosumer.js/:id/buffer_max', async (req, res) => {
+// Get prosumer max buffer by id
+router.get('/prosumer/:id/buffer_max', async (req, res) => {
     try {
         const prosumer = await server_db_utils.getProsumerById(req.params.id)
         if(prosumer == null){
@@ -155,8 +155,8 @@ router.get('/prosumer.js/:id/buffer_max', async (req, res) => {
     }
 })
 
-// Update prosumer.js max buffer by id
-router.put('/prosumer.js/:id/buffer_max', async (req, res) => {
+// Update prosumer max buffer by id
+router.put('/prosumer/:id/buffer_max', async (req, res) => {
     try{
         const prosumer = await server_db_utils.updateProsumerBufferSizeById(req.params.id, req.body.buffer_max)
         if (prosumer == null) {
