d9c7fd354cef231ce33091c858f891364cb2ee50
Edvin Sladic <edvinnn@users.noreply.github.com>
2019-12-03 13:25:21 +0100

    removed simulator/server.js (#51)
    * removed simulator/server.js
    
    * modiefied startscript to only run server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+require('../simulator/simulator')
 const express = require('express')
 const app = express()
 const passport = require('passport')
