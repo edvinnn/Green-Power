a9fbc40621b4c2a380a1e3094adfd90ba5dc95cd
hampusholmstrom <hampe.holmstrom@gmail.com>
2019-12-05 16:03:11 +0100

    slider functionality front end

--- server/website/routes.js
+++ server/website/routes.js
@@ -40,7 +40,7 @@ router.post('/register', async (req, res) => {
 })
 
 router.get('/dashboard', checkAuth, async (req, res) => {
-    res.render('dashboard.ejs', {user: req.user, ws: process.env.SERVER_WS_ADDRESS})
+    res.render('dashboard.ejs', {user: req.user, ws: process.env.SERVER_WS_ADDRESS, api: process.env.SERVER_ADDRESS})
 })
 
 router.get('/logout', checkAuth, (req, res) => {
